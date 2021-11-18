9d6ee5c44fad2ed816a8a01fddca0a1967b1606f
michela22 <michela.minniti@libero.it>
2021-11-18 09:54:31 +0100

    eliminazione prova commit

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,10 +1,10 @@
 export class User { 
     firstName: string;
      lastName: string;
-     fiscalCode: Number;
+   
      
      constructor(firstName: string, lastName: string, fiscalCode: Number) { 
          this.firstName = firstName; 
          this.lastName = lastName;
-         this.fiscalCode=fiscalCode;
+        
      } }
